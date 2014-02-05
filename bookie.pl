7409999fa5232ebb0d59022bf932d0fca67cc537
Mark Benson <markbenson@exosite.com>
2014-02-05 17:25:23 -0600

    Updated comments

--- bookie.pl
+++ bookie.pl
@@ -11,12 +11,12 @@ $src =~ s{^// ?javascript:.+\n}{};
 my $bookmarklet = $src;
 
 for ($bookmarklet) {
-    s{^\s*//.+\n}{}gm;  # Kill comments.
-    s{\t}{ }gm;         # Tabs to spaces
-    s{[ ]{2,}}{ }gm;    # Space runs to one space
-    s{^\s+}{}gm;        # Kill line-leading whitespace
-    s{\s+$}{}gm;        # Kill line-ending whitespace
-    s{\n}{}gm;          # Kill newlines
+    s{^\s*//.+\n}{}gm; # Remove comments
+    s{\t}{ }gm;        # Convert tabs to spaces
+    s{[ ]{2,}}{ }gm;   # Collapse spaces
+    s{^\s+}{}gm;       # Remove leading whitespace
+    s{\s+$}{}gm;       # Remove trailing whitespace
+    s{\n}{}gm;         # Remove newlines
 }
 
 $bookmarklet = "javascript:" .
